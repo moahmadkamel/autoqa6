70630ca0b7b0f1d72b5f46b47b445ef62e7e41e0
Mohamed Mohamed <mohamed.sep92@gmail.com>
2024-01-01 18:05:38 +0200

    maven setup

---
+++ maven-demo/src/main/java/com/example/driverFactory/DriverSetUp.java
@@ -0,0 +1,24 @@
+package com.example.driverFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+// import java.time.Duration;
+
+public class DriverSetUp {
+    public static WebDriver setUpDriver(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        // driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+        // driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(5));
+
+        return driver;
+    }
+}
