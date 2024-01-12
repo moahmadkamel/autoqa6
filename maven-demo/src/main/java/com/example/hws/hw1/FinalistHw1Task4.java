338d5d83f54027071b6cb30eba3720a6c0a7843c
Mohamed Mohamed <mohamed.sep92@gmail.com>
2024-01-13 01:48:05 +0200

    hw1 task4 update

--- maven-demo/src/main/java/com/example/hws/hw1/FinalistHw1Task4.java
+++ maven-demo/src/main/java/com/example/hws/hw1/FinalistHw1Task4.java
@@ -1,8 +1,10 @@
 package com.example.hws.hw1;
+
 import org.openqa.selenium.By;
-// import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebElement;
 import com.example.driverFactory.DriverSetUp;
 
 public class FinalistHw1Task4 {
@@ -14,28 +16,28 @@ public class FinalistHw1Task4 {
     */
 
     
-    /* 
+    
     public static void acceptCookies(WebDriver driver) throws InterruptedException {
         Thread.sleep(2000);
         WebElement acceptCookies = driver.findElement(Locators.acceptCookies);
         acceptCookies.click();
         Thread.sleep(5000);
     }
-    */
+    
     
     private static class Url {
         private static final String autoPractice = "http://www.automationpractice.pl/index.php";
     }
 
     private static class Locators {
-        // private static final By acceptCookies = By.id("");
+        private static final By acceptCookies = By.id("ez-accept-all");
         private static final By searchField = By.id("search_query_top");
         private static final By searchButton = By.name("submit_search");
-        private static final By resultSearchItem1 = By.xpath("(//a[@title='Printed Chiffon Dress'])[2]");
-        private static final By addItemToCart = By.name("Submit");
+        private static final By resultSearchItem1 = By.xpath("//div[@class='right-block'][1]/h5/a[@title='Printed Chiffon Dress']");
+        private static final By addItemToCart = By.xpath("//button[@name='Submit']");
         private static final By buttonContinue = By.xpath("//span[@title='Continue shopping']");
-        private static final By navItem1 = By.xpath("(//a[@title='Women'])[1]");
-        private static final By resultSearchItem2 = By.xpath("(//a[@title='Faded Short Sleeve T-shirts'])[2]");
+        private static final By navItem1 = By.xpath("//a[@title='Women'][1]");
+        private static final By resultSearchItem2 = By.xpath("//div[@class='right-block'][1]/h5/a[@title='Faded Short Sleeve T-shirts']");
         private static final By cart = By.xpath("//a[@title='View my shopping cart']");
     }
     
@@ -48,20 +50,29 @@ public class FinalistHw1Task4 {
         WebDriver driver = DriverSetUp.setUpDriver();
         driver.get(Url.autoPractice);
 
-        /*
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        
         try {
             acceptCookies(driver);
         } catch (NoSuchElementException e) {
             System.out.println("Cookies not found");
         }
-        */
+        
         
         driver.findElement(Locators.searchField).sendKeys(Data.inputForSearch1);
         Thread.sleep(4000);
         driver.findElement(Locators.searchButton).click();
         Thread.sleep(4000);
+
+        js.executeScript("window.scrollBy(0,300)");
+        Thread.sleep(3000);
+
         driver.findElement(Locators.resultSearchItem1).click();
         Thread.sleep(4000);
+
+        js.executeScript("window.scrollBy(0,100)");
+        Thread.sleep(3000);
+
         driver.findElement(Locators.addItemToCart).click();
         Thread.sleep(4000);
         driver.findElement(Locators.buttonContinue).click();
@@ -73,14 +84,26 @@ public class FinalistHw1Task4 {
         Thread.sleep(4000);
         driver.findElement(Locators.searchButton).click();
         Thread.sleep(4000);
+
+        js.executeScript("window.scrollBy(0,300)");
+        Thread.sleep(3000);
+
         driver.findElement(Locators.resultSearchItem2).click();
         Thread.sleep(4000);
+
+        js.executeScript("window.scrollBy(0,100)");
+        Thread.sleep(3000);
+
         driver.findElement(Locators.addItemToCart).click();
         Thread.sleep(4000);
         driver.findElement(Locators.buttonContinue).click();
         Thread.sleep(4000);
         driver.findElement(Locators.cart).click();
         Thread.sleep(4000);
+
+        js.executeScript("window.scrollBy(0,250)");
+        Thread.sleep(3000);
+
         driver.quit();
     }
 }
